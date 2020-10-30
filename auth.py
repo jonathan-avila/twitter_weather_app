163b88f5e03a951b52221527f259abbca6115256
Jonathan <jonathan.avila@nyu.edu>
2020-10-29 21:49:02 -0400

    edited weather.sh

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-consumer_key        = 'ijRfZAZDLyBs3dLXziz1eH2vs'
-consumer_secret     = 'X4IsGfC23GX8gHeheqvNtyNdODDD4RwWmFtfPD0OCzxuNJHyIL'
-access_token        = '1217559881546436608-9nnAPlamAIqtxzKD7UHzZU5vjhfBuU'
-access_token_secret = 'hF7JAZ2v7vqrJedR36GAWbEywKArmUfVpHWbIioEsnS2Q'
+consumer_key        = 
+consumer_secret     = 
+access_token        = 
+access_token_secret = 
